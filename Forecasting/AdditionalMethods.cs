b6db72154f1b0ff82b7bc89c99c1923413b7bae3
Bram Mulder <bram.mulder@parkmobile.com>
2017-03-21 18:38:13 +0100

    SES implementation

---
+++ Forecasting/AdditionalMethods.cs
@@ -0,0 +1,16 @@
+﻿namespace Forecasting
+{
+    public static class AdditionalMethods
+    {
+        public static double CalculateAlpha(double[] x)
+        {
+            var sum = 0.0;
+            for (int i = 0; i < 12; i++)
+            {
+                sum = sum + x[i];
+            }
+
+            return sum;
+        }
+    }
+}
